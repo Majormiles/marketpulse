b96de55f53583b6c0cb6c7793ab955c4ebfc8c57
Majoemyles <majormilesvigour@gmail.com>
2024-12-06 01:09:41 +0000

    Ready to deploy

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-const local = 'https://marketpulse-api.onrender.com'
-const production = ''
+// const local = 'https://marketpulse-api.onrender.com'
+const production = 'https://marketpulse-api.onrender.com'
 const api = axios.create({
-    baseURL: `${local}/api`,
+    baseURL: `${production}/api`,
     withCredentials : true
 })
 export default api
