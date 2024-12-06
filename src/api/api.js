4c557d9eefa16e087230cc5f9893830b152a15ab
Majoemyles <majormilesvigour@gmail.com>
2024-12-06 20:29:38 +0000

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // const local = 'https://marketpulse-api.onrender.com'
-const production = 'https://marketpulse-api.onrender.com/'
+const production = 'https://marketpulse-api.onrender.com'
 const api = axios.create({
     baseURL: `${production}/api`,
     withCredentials : true
