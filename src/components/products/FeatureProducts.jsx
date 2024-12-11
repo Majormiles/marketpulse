56918edd4b64f07d790faec2b46f6c634a6defb2
Majoemyles <majormilesvigour@gmail.com>
2024-12-11 11:19:14 +0000

    update

--- src/components/products/FeatureProducts.jsx
+++ src/components/products/FeatureProducts.jsx
@@ -59,11 +59,15 @@ const FeatureProducts = ({ products }) => {
             <div className='w-full grid grid-cols-4 md-lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6'>
                 {
                     products.map((p, i) => <div key={i} className='border group transition-all duration-500 hover:shadow-md hover:-mt-3'>
-                        <div className='relative overflow-hidden'>
+                        <div className='relative overflow-hidden p-5'>
                             {
                                 p.discount ? <div className='flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2'>{p.discount}%</div> : ""
                             }
-                            <img className='sm:w-[full] w-[230px] h-[155px]' src={p.images[0]} alt="product image" />
+                             <img
+                                className='sm:w-full w-[220px] h-[165px] mx-auto rounded-[10px]'
+                                src={p.images[0]}
+                                alt="product image"
+                            />
                             <ul className='flex transition-all duration-700 -bottom-10 justify-center items-center gap-2 absolute w-full group-hover:bottom-3'>
                                 <li onClick={() => add_wishlist(p)} className='w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#7fad39] hover:text-white hover:rotate-[720deg] transition-all'><AiFillHeart /></li>
                                 <Link to={`/product/details/${p.slug}`} className='w-[30px] h-[30px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#7fad39] hover:text-white hover:rotate-[720deg] transition-all' ><FaEye /></Link>
